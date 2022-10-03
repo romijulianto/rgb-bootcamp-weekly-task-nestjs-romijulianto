9cdeba38713176e5967df2e4c12c591afbd04095
Romi Julianto <romyjulians@gmail.com>
2022-10-04 00:10:57 +0700

    add method crud clinic

--- src/modules/clinics/clinics.controller.ts
+++ src/modules/clinics/clinics.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, NotFoundException, UseGuards, Request } from '@nestjs/common';
+import { Clinic as ClinicEntity } from './clinics.entity';
+import { ClinicsService } from './clinics.service';
 
 @Controller('clinics')
-export class ClinicsController {}
+export class ClinicsController {
+    constructor(private readonly clinicService: ClinicsService) { }
+
+    @Get()
+    async findAll() {
+        /* get all clinics in the database */
+        return await this.clinicService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<ClinicEntity> { 
+        /* find the clinic with id */
+        const clinic = await this.clinicService.findOne(id);
+
+        /* if the clinic doesn't exist in db, thr0w 404 */
+        if (!clinic) {
+            throw new NotFoundException('This Clinic doesn\'t exist');
+        }
+        /* if clinic exist, return clinic */
+        return clinic;
+    }
+
+    
+}
