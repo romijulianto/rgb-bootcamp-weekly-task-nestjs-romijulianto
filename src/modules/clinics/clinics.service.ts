6525d7bc1ca67b983f37b7e76220cb91cbf126a3
Romi Julianto <romyjulians@gmail.com>
2022-10-04 00:01:00 +0700

    add method crud to clinics service

--- src/modules/clinics/clinics.service.ts
+++ src/modules/clinics/clinics.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { CLINIC_REPOSITORY } from 'src/core/constants';
+import { User } from '../users/user.entity';
+import { Clinic } from './clinics.entity';
+import { ClinicDto } from './dto/clinics.dto';
 
 @Injectable()
-export class ClinicsService {}
+export class ClinicsService {
+    constructor(@Inject(CLINIC_REPOSITORY)
+    private readonly clinicRepository: typeof Clinic) { }
+
+    async create(clinic: ClinicDto, userId): Promise<Clinic> {
+        return await this.clinicRepository.create<Clinic>({ ...clinic, userId });
+    }
+
+    async findAll(): Promise<Clinic[]> {
+        return await this.clinicRepository.findAll<Clinic>({
+            include: [{ model: User, attributes: { exclude: ['password'] } }],
+        });
+    }
+
+    async findOne(id): Promise<Clinic> {
+        return await this.clinicRepository.findOne({
+            where: { id },
+            include: [{ model: User, attributes: { exclude: ['password'] } }],
+        });
+    }
+
+    async delete(id, userId) {
+            return await this.clinicRepository.destroy({
+                where: { id, userId }
+            });
+    }
+
+    async update(id, data, userId) {
+        const [numberOfAffectedRows, [updatedClinic]] = await this.clinicRepository.update(
+            { ...data },
+            {
+                where: { id, userId },
+                returning: true
+            });
+    }
+}
